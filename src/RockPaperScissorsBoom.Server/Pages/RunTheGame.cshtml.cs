49055bb10437a55a4835d3642cdbfd4ea924c2e5
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-05-24 13:07:42 -0400

    Mocking up the results page

---
+++ src/RockPaperScissorsBoom.Server/Pages/RunTheGame.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RockPaperScissorsBoom.Server.Pages
+{
+    public class RunTheGameModel : PageModel
+    {
+        public void OnGet()
+        {
+            BotRankings = new List<BotRanking>
+            {
+                new BotRanking { BotName = "Bot1", Rank = 1, Wins = 3, Losses = 0 },
+                new BotRanking { BotName = "Bot3", Rank = 2, Wins = 2, Losses = 1 },
+                new BotRanking { BotName = "Bot4", Rank = 4, Wins = 0, Losses = 3 },
+                new BotRanking { BotName = "Bot2", Rank = 3, Wins = 1, Losses = 2 },
+            };
+        }
+
+        public IEnumerable<BotRanking> BotRankings { get; set; }
+    }
+
+    public class BotRanking
+    {
+        public string BotName { get; set; }
+        public int Rank { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+    }
+}
