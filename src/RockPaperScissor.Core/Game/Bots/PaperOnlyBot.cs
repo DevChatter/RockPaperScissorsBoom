76159c4e18a548df7264771c995f59636053ed86
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-05-24 15:23:13 -0400

    Add more simple bots

---
+++ src/RockPaperScissor.Core/Game/Bots/PaperOnlyBot.cs
@@ -0,0 +1,15 @@
+﻿using RockPaperScissor.Core.Game.Results;
+
+namespace RockPaperScissor.Core.Game.Bots
+{
+    public class PaperOnlyBot : IBot
+    {
+        public PaperOnlyBot(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+        public Decision GetDecision(RoundResult previousResult) => Decision.Paper;
+    }
+}
