94769982cccabd16159f8bb130ebf22647e0c430
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-05-24 15:02:17 -0400

    Add code to determine winning decisions

---
+++ src/RockPaperScissor.Core/Extensions/DecisionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RockPaperScissor.Core.Game;
+
+namespace RockPaperScissor.Core.Extensions
+{
+    public static class DecisionExtensions
+    {
+        public static bool IsWinnerAgainst(this Decision d1, Decision d2)
+        {
+            switch (d1)
+            {
+                case Decision.Rock:
+                    return d2 == Decision.Scissors || d2 == Decision.WaterBalloon;
+                case Decision.Paper:
+                    return d2 == Decision.Rock || d2 == Decision.WaterBalloon;
+                case Decision.Scissors:
+                    return d2 == Decision.Paper || d2 == Decision.WaterBalloon;
+                case Decision.WaterBalloon:
+                    return d2 == Decision.Dynamite;
+                case Decision.Dynamite:
+                    return d2 == Decision.Paper || d2 == Decision.Scissors || d2 == Decision.Rock;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
