34a4ecd919e473621387e24ae2fbfd74cfcd5d63
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-05-24 15:23:35 -0400

    Add simple win tests for RoundRunner

--- src/UnitTests/Core/Game/RoundRunnerTests/RoundRunnerShould.cs
+++ src/UnitTests/Core/Game/RoundRunnerTests/RoundRunnerShould.cs
@@ -9,6 +9,9 @@ namespace UnitTests.Core.Game.RoundRunnerTests
     public class RoundRunnerShould
     {
         private readonly RockOnlyBot _rockOnlyBot = new RockOnlyBot("Rocky");
+        private readonly PaperOnlyBot _paperOnlyBot = new PaperOnlyBot("Paper");
+        private readonly WaterOnlyBot _waterOnlyBot = new WaterOnlyBot("Water");
+        private readonly DynamiteOnlyBot _dynamiteOnlyBot = new DynamiteOnlyBot("Dyna");
         private readonly ScissorsOnlyBot _scissorsOnlyBot = new ScissorsOnlyBot("Cutter");
         private readonly RoundRunner _roundRunner = new RoundRunner();
 
@@ -26,11 +29,30 @@ namespace UnitTests.Core.Game.RoundRunnerTests
         }
 
         [Fact]
-        public void ReturnTie_GivenSameChoice()
+        public void ReturnNoWinner_GivenSameChoice()
         {
             RoundResult roundResult = _roundRunner.RunRound(_rockOnlyBot, _rockOnlyBot, new RoundResult());
 
             roundResult.Winner.Should().BeNull();
+        }
+
+        [Fact]
+        public void ReturnCorrectWinner_GivenSimpleWins()
+        {
+            RoundResult rockWin = _roundRunner.RunRound(_rockOnlyBot, _scissorsOnlyBot, new RoundResult());
+            rockWin.Winner.Should().Be(_rockOnlyBot);
+
+            RoundResult paperWin = _roundRunner.RunRound(_paperOnlyBot, _rockOnlyBot, new RoundResult());
+            paperWin.Winner.Should().Be(_paperOnlyBot);
+
+            RoundResult scissorsWin = _roundRunner.RunRound(_scissorsOnlyBot, _paperOnlyBot, new RoundResult());
+            scissorsWin.Winner.Should().Be(_scissorsOnlyBot);
+
+            RoundResult dynamiteWin = _roundRunner.RunRound(_dynamiteOnlyBot, _rockOnlyBot, new RoundResult());
+            dynamiteWin.Winner.Should().Be(_dynamiteOnlyBot);
+
+            RoundResult waterWin = _roundRunner.RunRound(_waterOnlyBot, _dynamiteOnlyBot, new RoundResult());
+            waterWin.Winner.Should().Be(_waterOnlyBot);
         }
     }
 }
