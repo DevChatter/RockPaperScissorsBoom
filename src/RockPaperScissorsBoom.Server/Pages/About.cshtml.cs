7451b1a9f263b8bb9934384698663762a0e59ebb
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-05-24 12:41:16 -0400

    Adding a rules page; clearing up other pages

--- src/RockPaperScissorsBoom.Server/Pages/About.cshtml.cs
+++ src/RockPaperScissorsBoom.Server/Pages/About.cshtml.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RockPaperScissorsBoom.Server.Pages
 {
     public class AboutModel : PageModel
     {
-        public string Message { get; set; }
-
         public void OnGet()
         {
-            Message = "Your application description page.";
         }
     }
 }
