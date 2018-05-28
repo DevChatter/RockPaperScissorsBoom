9f648f62f9acce417e5f0e89094a1a8ef16c3c1f
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-05-28 14:39:09 -0400

    Adding an Example Bot implementation

---
+++ src/RockPaperScissorsBoom.ExampleBot/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RockPaperScissorsBoom.ExampleBot.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            JsonData = "{Choice:\"Rock\"}";
+        }
+
+        public string JsonData { get; set; }
+    }
+}
