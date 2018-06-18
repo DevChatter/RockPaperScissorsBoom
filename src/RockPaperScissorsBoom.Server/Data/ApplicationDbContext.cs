0c497835416eb01fc07fe7d48520dea6e32a8999
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-06-18 15:17:03 -0400

    Create BotRecord table

--- src/RockPaperScissorsBoom.Server/Data/ApplicationDbContext.cs
+++ src/RockPaperScissorsBoom.Server/Data/ApplicationDbContext.cs
@@ -8,19 +8,12 @@ namespace RockPaperScissorsBoom.Server.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         //public DbSet<FullResults> FullResults { get; set; }
-        //public DbSet<BotRecord> BotRecords { get; set; }
+        public DbSet<BotRecord> BotRecords { get; set; }
         public DbSet<Competitor> Competitors { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-
-
-            base.OnModelCreating(builder);
         }
     }
 }
---
+++ src/RockPaperScissorsBoom.Server/Data/Migrations/20180618191300_CreateBotRecordTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RockPaperScissorsBoom.Server.Data.Migrations
+{
+    public partial class CreateBotRecordTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BotRecords",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CompetitorId = table.Column<Guid>(nullable: true),
+                    Wins = table.Column<int>(nullable: false),
+                    Losses = table.Column<int>(nullable: false),
+                    Ties = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BotRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BotRecords_Competitors_CompetitorId",
+                        column: x => x.CompetitorId,
+                        principalTable: "Competitors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BotRecords_CompetitorId",
+                table: "BotRecords",
+                column: "CompetitorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BotRecords");
+        }
+    }
+}
